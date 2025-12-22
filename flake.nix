49ff612f785be7416f8a2d21f2735fa9df514c19
Luke Bailey <baileylu@tcd.ie>
2025-12-22 17:36:41 +0000

    Init repo with basic flake for rust dev

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Container PID 1 and process runner for Nix Modular Services";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./nix);
+}
---
+++ nix/dev/devshell.nix
@@ -0,0 +1,34 @@
+{
+  perSystem =
+    { pkgs, self', ... }:
+    let
+      inherit (pkgs) lib stdenv;
+
+      moldHook =
+        pkgs.makeSetupHook
+          {
+            name = "mold-hook";
+
+            propagatedBuildInputs = with pkgs; [
+              mold
+            ];
+          }
+          (
+            pkgs.writeText "moldHook.sh" ''
+              export RUSTFLAGS="-C link-arg=-fuse-ld=mold"
+            ''
+          );
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          rustfmt
+          clippy
+
+          (lib.optional (!stdenv.isDarwin) moldHook)
+        ];
+      };
+    };
+}
