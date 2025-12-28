71b0363f37433f6fdcc6887c3c12111a8339b857
Luke Bailey <baileylu@tcd.ie>
2025-12-28 18:20:30 +0000

    Add nix config options for configuring process manager settings

--- nix/_nimi-module.nix
+++ nix/_nimi-module.nix
@@ -5,6 +5,49 @@
   ...
 }:
 let
+  restartType = types.submodule {
+    options = {
+      mode = mkOption {
+        description = ''
+          The restart mode to use for nimi
+        '';
+        default = "always";
+        type = types.enum [
+          "never"
+          "up-to-count"
+          "always"
+        ];
+      };
+      time = mkOption {
+        description = ''
+          Amount of time (ms) to wait between process restarts
+        '';
+        type = types.ints.positive;
+        default = 10;
+      };
+      count = mkOption {
+        description = ''
+          If `mode` == `up-to-count`, the maximum amount of times to restart
+          before exiting
+        '';
+        type = types.ints.positive;
+        default = 5;
+      };
+    };
+  };
+
+  settingsType = types.submodule {
+    options = {
+      restart = mkOption {
+        description = ''
+          Decides how nimi should be restarted
+        '';
+        type = restartType;
+        default = { };
+      };
+    };
+  };
+
   inherit (lib) mkOption types;
 in
 {
@@ -24,5 +67,13 @@ in
     );
     default = { };
     visible = "shallow";
+  };
+
+  options.settings = mkOption {
+    description = ''
+      Settings for the nimi instance itself
+    '';
+    type = settingsType;
+    default = { };
   };
 }
