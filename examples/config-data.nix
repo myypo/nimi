a696c849da365925adc928e8c6e19f66c9c6c6e4
Luke Bailey <baileylu@tcd.ie>
2026-01-11 17:39:43 +0000

    Wire up examples dir too

--- examples/config-data.nix
+++ examples/config-data.nix
@@ -1,22 +1,20 @@
-{ lib, ... }:
 {
-  perSystem =
-    { pkgs, self', ... }:
-    {
-      checks.configData = self'.packages.nimi.mkNimiBin {
-        services."has-config-data" = {
-          process.argv = [
-            (lib.getExe pkgs.http-server)
-          ];
-          configData."my-config-file" = {
-            enable = true;
-            text = ''
-              hello world
-            '';
-            path = "my-config-file.txt";
-          };
-        };
-        settings.restart.mode = "always";
-      };
+  nimi,
+  lib,
+  http-server,
+}:
+nimi.mkNimiBin {
+  services."has-config-data" = {
+    process.argv = [
+      (lib.getExe http-server)
+    ];
+    configData."my-config-file" = {
+      enable = true;
+      text = ''
+        hello world
+      '';
+      path = "my-config-file.txt";
     };
+  };
+  settings.restart.mode = "always";
 }
--- examples/ghostunnel.nix
+++ examples/ghostunnel.nix
@@ -1,34 +1,31 @@
 # This package seems to be used as the testbed for development of Modular Services in `nixpkgs`
-
 {
-  perSystem =
-    { pkgs, self', ... }:
-    {
-      checks.ghostunnel = self'.packages.nimi.mkNimiBin {
-        services."ghostunnel-plain-old" = {
-          imports = [ pkgs.ghostunnel.services.default ];
-          ghostunnel = {
-            listen = "0.0.0.0:443";
-            cert = "/root/service-cert.pem";
-            key = "/root/service-key.pem";
-            disableAuthentication = true;
-            target = "backend:80";
-            unsafeTarget = true;
-          };
-        };
-        services."ghostunnel-client-cert" = {
-          imports = [ pkgs.ghostunnel.services.default ];
-          ghostunnel = {
-            listen = "0.0.0.0:1443";
-            cert = "/root/service-cert.pem";
-            key = "/root/service-key.pem";
-            cacert = "/root/ca.pem";
-            target = "backend:80";
-            allowCN = [ "client" ];
-            unsafeTarget = true;
-          };
-        };
-        settings.restart.mode = "up-to-count";
-      };
+  nimi,
+  ghostunnel,
+}:
+nimi.mkNimiBin {
+  services."ghostunnel-plain-old" = {
+    imports = [ ghostunnel.services.default ];
+    ghostunnel = {
+      listen = "0.0.0.0:443";
+      cert = "/root/service-cert.pem";
+      key = "/root/service-key.pem";
+      disableAuthentication = true;
+      target = "backend:80";
+      unsafeTarget = true;
     };
+  };
+  services."ghostunnel-client-cert" = {
+    imports = [ ghostunnel.services.default ];
+    ghostunnel = {
+      listen = "0.0.0.0:1443";
+      cert = "/root/service-cert.pem";
+      key = "/root/service-key.pem";
+      cacert = "/root/ca.pem";
+      target = "backend:80";
+      allowCN = [ "client" ];
+      unsafeTarget = true;
+    };
+  };
+  settings.restart.mode = "up-to-count";
 }
